d21f1ad2a0e289e2b54c13b84c7d65b0b150560f
liam <380639608@qq.com>
2020-10-10 23:52:42 +0800

    通过监听器将springIoc容器存放在ServletContext中

--- ch10-spring-web/src/main/java/com/lea/controller/RegisterServlet.java
+++ ch10-spring-web/src/main/java/com/lea/controller/RegisterServlet.java
@@ -4,6 +4,7 @@ import com.lea.domain.Student;
 import com.lea.service.StudentService;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 
 import javax.servlet.ServletContext;
 import java.io.IOException;
@@ -21,12 +22,16 @@ public class RegisterServlet extends javax.servlet.http.HttpServlet {
 //        String config = "applicationContext.xml";
 //        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(config);
         WebApplicationContext context = null;
-        // 从servlet容器中拿到创建好的ApplicationContext对象
-        ServletContext servletContext = request.getServletContext();
-        Object attribute = servletContext.getAttribute(WebApplicationContext.ROOT_WEB_APPLICATION_CONTEXT_ATTRIBUTE);
-        if (null != attribute) {
-            context = (WebApplicationContext) attribute;
-        }
+        // 2.从servlet容器中拿到创建好的ApplicationContext对象
+//        ServletContext servletContext = request.getServletContext();
+//        Object attribute = servletContext.getAttribute(WebApplicationContext.ROOT_WEB_APPLICATION_CONTEXT_ATTRIBUTE);
+//        if (null != attribute) {
+//            context = (WebApplicationContext) attribute;
+//        }
+        // 3. 从框架工具类中拿到ServletContext,再获取容器对象
+        ServletContext servletContext = getServletContext();
+        context = WebApplicationContextUtils.getWebApplicationContext(servletContext);
+
         StudentService studentService = context.getBean("studentService", StudentService.class);
         System.out.println(" 创建的对象" + studentService.getClass().getName());
         Student student = new Student(Integer.valueOf(id), name, email, Integer.valueOf(age));
