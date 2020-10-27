62eaa4ad08f9e363a6c5d9feba6523a8055a238c
liam <380639608@qq.com>
2020-10-27 16:32:45 +0800

    spring扩展原理汇总

--- ch14-spring-annotation/src/main/java/com/lea/ext/ExtConfig.java
+++ ch14-spring-annotation/src/main/java/com/lea/ext/ExtConfig.java
@@ -1,9 +1,11 @@
 package com.lea.ext;
 
 import com.lea.bean.Person;
+import org.springframework.context.ApplicationEvent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.EventListener;
 
 /**
  * @author lzc
@@ -35,13 +37,13 @@ import org.springframework.context.annotation.Configuration;
  * 3.ApplicationListener：监听容器中发布的事件。事件驱动模型开发
  *      public interface ApplicationListener<E extends ApplicationEvent> extends EventListener
  *          监听 ApplicationEvent 及其下面的子事件：
- *     开发监听器步骤：
+ *     开发监听器步骤： （@EventListener 可以给任意方法，实现事件监听，不用重写接口） --看实现类-》 EventListenerMethodProcessor
  *      1.写一个监听器来监听每个事件（ApplicationEvent及其子类）
  *      2.把监听器加入容器中
  *      3.只要容器中有相关事件的发布，我们就能监听到这个事件
  *          eg：ContextRefreshedEvent：容器刷新完成（所有bean都完全创建），spring会发布这个事件
  *              ContextClosedEvent：spring关闭时，会发布此事件
- *      4. 如果发布事件
+ *      4. 怎么发布事件
  *           通过容器 ac.publishEvent(new ApplicationEvent(new String(" 这是一个事件")) {});
  * 原理：
  *  ContextRefreshedEvent、com.lea.TestExt$1[source= 这是一个事件]、ContextClosedEvent：
@@ -73,6 +75,15 @@ import org.springframework.context.annotation.Configuration;
  *      for (String listenerBeanName : listenerBeanNames) {
  * 			getApplicationEventMulticaster().addApplicationListenerBean(listenerBeanName);  // 注册
  *       }
+ *
+ *  （@EventListener）SmartInitializingSingleton 原理：  --- 》afterSingletonsInstantiated();
+ *      1.容器创建对象 refresh();
+ *      2. finishBeanFactoryInitialization(beanFactory);   初始化所有的单实例bean
+ *          1. 先创建所有单实例bean getBean()
+ *          2. 获取所有创建好的单实例bean，判断是否是  SmartInitializingSingleton
+ *              是就调用afterSingletonsInstantiated();
+ *
+ *
  */
 @Configuration
 @ComponentScan("com.lea.ext")
@@ -81,5 +92,11 @@ public class ExtConfig {
     @Bean
     public Person person() {
         return new Person();
+    }
+
+    // 监听事件
+    @EventListener(classes = ApplicationEvent.class)
+    public void listener(ApplicationEvent event) {
+        System.out.println("listener 方法监听到的信息：" + event);
     }
 }
